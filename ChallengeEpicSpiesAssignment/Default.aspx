f67172a6a9471b53aac77993b9958863bf491759
DKelley <dianna.kelley@gmail.com>
2018-01-31 17:41:28 -0800

    Add project files.

---
+++ ChallengeEpicSpiesAssignment/Default.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeEpicSpiesAssignment.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 242px;
+            height: 300px;
+        }
+        .auto-style2 {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <img class="auto-style1"  src="epic-spies-logo.jpg" height="190" /></div>
+        <h2 class="auto-style2">Spy New Assignment Form</h2>
+        <p>
+            Spy Code Name:&nbsp;
+            <asp:TextBox ID="spyCodeTextBox" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            New Assignment Name:&nbsp;
+            <asp:TextBox ID="newAssignmentNameTextBox" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            End Date of Previous Assignment:</p>
+        <p>
+            <asp:Calendar ID="previousEndDateCalendar" runat="server"></asp:Calendar>
+        </p>
+        <p>
+            Start Date of New Assignment:</p>
+        <p>
+            <asp:Calendar ID="newStartDateCalendar" runat="server"></asp:Calendar>
+        </p>
+        <p>
+            Projected End Date of New Assignment:</p>
+        <p>
+            <asp:Calendar ID="projectedNewEndDateCalendar" runat="server"></asp:Calendar>
+        </p>
+        <p>
+            <asp:Button ID="assignSpyButton" runat="server" OnClick="assignSpyButton_Click" Text="Assign Spy" />
+        </p>
+        <p>
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ ChallengeEpicSpiesAssignment/Default.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeEpicSpiesAssignment
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                DateTime previousEnd = previousEndDateCalendar.TodaysDate;
+                DateTime startNew = previousEnd.AddDays(14);
+                DateTime newEnd = previousEnd.AddDays(21);
+            }
+
+        }
+
+        protected void assignSpyButton_Click(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                DateTime newpreviousEnd = previousEndDateCalendar.SelectedDate;
+                DateTime newStartNew = newStartDateCalendar.SelectedDate;
+                DateTime newEndDate = projectedNewEndDateCalendar.SelectedDate;
+
+               
+                int shortDate = newpreviousEnd.Subtract(newStartNew).Days;
+
+                int jobdays = newEndDate.Subtract(newStartNew).Days;
+                int dayWage = 500;
+                int jobBonus = 1000;
+                int paycheck = jobdays * dayWage;
+
+
+
+                if (jobdays >= 14 && jobdays <= 20)
+
+                {
+                    double result = paycheck;
+
+                    string result2 = String.Format("Assignment of  {0} to assignment Project: {1}  for {2:C}",
+                        spyCodeTextBox.Text,
+                        newAssignmentNameTextBox.Text,
+                        result);
+
+                    resultLabel.Text = result2;
+                }
+
+                else if (jobdays >= 21)
+                {
+                    double result = (paycheck + jobBonus);
+
+                    string result2 = String.Format("Assignment of  {0} to assignment Project: {1}  for {2:C}",
+                        spyCodeTextBox.Text,
+                        newAssignmentNameTextBox.Text,
+                        result);
+
+                    resultLabel.Text = result2;
+                }
+                else if (shortDate < 14)
+                {
+                    resultLabel.Text =
+                        "Error: Must allow at least two weeks between previous assignment and the new assignment.";
+                }
+            }
+
+
+
+        }
+
+    }
+}
